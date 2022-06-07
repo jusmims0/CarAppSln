a323959cd902e8f2fd6299984945d93d489172ec
jusmims0 <102726528+jusmims0@users.noreply.github.com>
2022-06-07 09:03:50 +0200

    Add project files.

---
+++ CarsApp/CarsApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CarsApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private List<Car> _car = new List<Car>();
+
+
+        public List<Car> Car
+            { get { return _car; } 
+            set { _car = value; }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
